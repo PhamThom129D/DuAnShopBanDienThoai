00048903e51abe1682f8fa98d03f4a521c184f71
T14 <vykid9@gmail.com>
2024-10-15 22:57:58 +0700

    ConnectDB Login

--- src/main/java/com/example/duan/Test.java
+++ src/main/java/com/example/duan/Test.java
@@ -1,2 +1,27 @@
-package com.example.duan;public class Test {
+package com.example.duan;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Test {
+    Connection connection = DatabaseConnection.getConnection();
+    Statement statement = connection.createStatement();
+
+    public Test() throws SQLException {
+    }
+    public void show(String query) throws SQLException {
+        ResultSet resultSet = statement.executeQuery(query);
+        while (resultSet.next()) {
+            String role = resultSet.getString("role");
+            String state = resultSet.getString("state");
+            String username = resultSet.getString("username");
+            String password = resultSet.getString("password");
+            String address = resultSet.getString("address");
+            String phoneNumber = resultSet.getString("phoneNumber");
+            System.out.println("Thông tin khách hàng : "+role+" "+state+" "+username+" "+password+" "+address+" "+phoneNumber);
+        }
+    }
+
 }
