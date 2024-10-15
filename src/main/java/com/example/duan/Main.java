ca7b88e6e1887bb20fc28b355b66415e88ec9d77
T14 <vykid9@gmail.com>
2024-10-16 01:56:33 +0700

    Update Login Sign

--- src/main/java/com/example/duan/Main.java
+++ src/main/java/com/example/duan/Main.java
@@ -2,19 +2,25 @@ package com.example.duan;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.sql.SQLException;
 
 public class Main extends Application {
+    private static Stage primaryStage;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 680, 480);
-        stage.setTitle("DuAnBanDienThoai!");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Sign.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        primaryStage.setMaximized(true);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        primaryStage.setTitle("DuAnBanDienThoai!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) throws SQLException {
@@ -22,5 +28,14 @@ public class Main extends Application {
         Test test = new Test();
         String query = "Select * from User";
         test.show(query);
+    }
+
+    public static void changeScene(String fxml) throws IOException {
+        Parent root = FXMLLoader.load(Main.class.getResource(fxml));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(Main.class.getResource("style.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.setMaximized(true);
+        primaryStage.show();
     }
 }
